a24fcc4414af33b1858fcf63c74688da617471b3
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-11 18:21:18 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ class Anagram
   def self.match(words)
     letter_array = @word.split("")
     new_letter_arr = letter_array.sort 
-    new_letter_arr.each do {|word| 
+    new_letter_arr.each do {|word|}
   end 
   
   
