5bf15210a733a87607a0f56350d4f7fb00dbc6b7
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-11 18:27:18 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,8 @@ class Anagram
   
   def match(word_array)
     word_array.select do |word|
-      word.split("").sort == @word.split
+      word.split("").sort == @word.split("").sort 
+      
    
   end 
   
