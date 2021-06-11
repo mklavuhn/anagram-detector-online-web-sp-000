5b438a1f56917a0b9a0ce07ec2bd5498b079d884
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-11 18:13:42 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,15 @@
 # Your code goes here!
 class Anagram 
   
-  
+  attr_accessor :word 
   
   def initialize(word)
     @word = word
   end
   
-  
+  def self.match(words)
+    
+  end 
   
   
 end
